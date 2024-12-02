a8df08494659195fafc64d9303c8fbfb2984a28c
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-12-01 20:51:45 -0800

    [Day 01] Part 2 - Naive approach.

--- src/Day01.kt
+++ src/Day01.kt
@@ -23,7 +23,22 @@ fun main() {
             .fold(0) { acc, pair -> acc + abs(pair.first - pair.second) }
     }
 
+    fun part2(input: List<String>): Int {
+        val first = mutableListOf<Int>()
+        val map = hashMapOf<Int, Int>()
+
+        input.asSequence()
+            .map { it.split(Regex("\\s+")) }
+            .filter { it.isNotEmpty() }
+            .map { it[0].toInt() to it[1].toInt() }
+            .onEach { first.add(it.first) }
+            .onEach { map[it.second] = (map[it.second] ?: 0) + 1 }
+            .count()
+
+        return first.fold(0) { acc, num -> acc + num * (map[num] ?: 0) }
+    }
+
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("input/Day01")
-    part1(input).println()
+    part2(input).println()
 }
