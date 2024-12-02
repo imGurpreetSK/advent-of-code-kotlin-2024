f3c48934abfedd5246ed43a594de82d4f7ee75ba
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-12-02 13:31:00 -0800

    [Day 02] Part 2.

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,8 +1,12 @@
 fun main() {
+
     fun part1(input: List<String>): Int = input.count { isSafe(it.toIntList()) }
+
+    fun part2(input: List<String>): Int = input.count { isLenientSafe(it.toIntList()) }
 
     val input = readInput("input/Day02")
     part1(input).println()
+    part2(input).println()
 }
 
 private fun isSafe(input: List<Int>): Boolean {
@@ -10,4 +14,8 @@ private fun isSafe(input: List<Int>): Boolean {
             input.windowed(2).all { (a, b) -> a < b && b - a in 1..3 }
 }
 
+private fun isLenientSafe(input: List<Int>): Boolean = isSafe(input) || input.indices.any { isSafe(input.omit(it)) }
+
 private fun String.toIntList(): List<Int> = split(Regex("\\s+")).map { it.toInt() }
+
+private fun List<Int>.omit(index: Int): List<Int> = filterIndexed { idx, _ -> idx != index }
