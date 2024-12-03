8dc03eddd145703c685ec70cdbd36f6ece442362
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-12-03 11:52:52 -0800

    [Day 03] Part 1.

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,10 +1,21 @@
+import java.io.BufferedReader
+import kotlin.io.path.*
+
 fun main() {
 
-    fun part1(input: List<String>): Int = TODO()
+    fun part1(inputFile: BufferedReader): Int = inputFile.useLines {
+        it.sumOf { findValidOccurrences(it) }
+    }
 
-    fun part2(input: List<String>): Int = TODO()
+    fun part2(input: BufferedReader): Int = TODO()
 
-    val input = readInput("input/Day03")
+    val input = Path("src/input/Day03.txt").reader().buffered(2048)
     part1(input).println()
     part2(input).println()
+}
+
+private fun findValidOccurrences(input: String): Int {
+    return Regex("""mul\((\d+),(\d+)\)""").findAll(input)
+        .map { it.groupValues[1].toInt() * it.groupValues[2].toInt() }
+        .sum()
 }
