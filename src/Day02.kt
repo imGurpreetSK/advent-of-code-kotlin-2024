28c2fd3299b7c0538a4c3b995590cceb01afff7e
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-12-03 11:08:28 -0800

    chore: prep for Day 3.

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,21 +1,23 @@
-fun main() {
+class Day02 {
 
-    fun part1(input: List<String>): Int = input.count { isSafe(it.toIntList()) }
+    fun main() {
+        fun part1(input: List<String>): Int = input.count { isSafe(it.toIntList()) }
 
-    fun part2(input: List<String>): Int = input.count { isLenientSafe(it.toIntList()) }
+        fun part2(input: List<String>): Int = input.count { isLenientSafe(it.toIntList()) }
 
-    val input = readInput("input/Day02")
-    part1(input).println()
-    part2(input).println()
+        val input = readInput("input/Day02")
+        part1(input).println()
+        part2(input).println()
+    }
+
+    private fun isSafe(input: List<Int>): Boolean {
+        return input.windowed(2).all { (a, b) -> a > b && a - b in 1..3 } ||
+                input.windowed(2).all { (a, b) -> a < b && b - a in 1..3 }
+    }
+
+    private fun isLenientSafe(input: List<Int>): Boolean = isSafe(input) || input.indices.any { isSafe(input.omit(it)) }
+
+    private fun String.toIntList(): List<Int> = split(Regex("\\s+")).map { it.toInt() }
+
+    private fun List<Int>.omit(index: Int): List<Int> = filterIndexed { idx, _ -> idx != index }
 }
-
-private fun isSafe(input: List<Int>): Boolean {
-    return input.windowed(2).all { (a, b) -> a > b && a - b in 1..3 } ||
-            input.windowed(2).all { (a, b) -> a < b && b - a in 1..3 }
-}
-
-private fun isLenientSafe(input: List<Int>): Boolean = isSafe(input) || input.indices.any { isSafe(input.omit(it)) }
-
-private fun String.toIntList(): List<Int> = split(Regex("\\s+")).map { it.toInt() }
-
-private fun List<Int>.omit(index: Int): List<Int> = filterIndexed { idx, _ -> idx != index }
---
+++ src/Day03.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+    fun part1(input: List<String>): Int = TODO()
+
+    fun part2(input: List<String>): Int = TODO()
+
+    val input = readInput("input/Day03")
+    part1(input).println()
+    part2(input).println()
+}
