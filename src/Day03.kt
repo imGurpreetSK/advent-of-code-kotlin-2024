76fe6b30bc6724fd47001002f64b3b24dc998ed4
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-12-03 15:53:36 -0800

    [Day 03] Part 2 - first pass.

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,17 +1,57 @@
 import java.io.BufferedReader
-import kotlin.io.path.*
+import kotlin.io.path.Path
+import kotlin.io.path.reader
 
 fun main() {
 
-    fun part1(inputFile: BufferedReader): Int = inputFile.useLines {
+    fun part1(reader: BufferedReader): Int = reader.useLines {
         it.sumOf { findValidOccurrences(it) }
     }
 
-    fun part2(input: BufferedReader): Int = TODO()
+    fun part2(input: String): Int {
+        val firstWindow = firstWindow(input)
+        return findValidOccurrences(firstWindow) +
+                otherWindows(input.replace(firstWindow, "")).sum()
+    }
 
-    val input = Path("src/input/Day03.txt").reader().buffered(2048)
-    part1(input).println()
-    part2(input).println()
+    part1(Path("src/sample/Day03_part2.txt").reader().buffered(2048)).println()
+    part2(readInput("input/Day03").joinToString(separator = "")).println()
+}
+
+private fun firstWindow(input: String): String {
+    val windowEnd = input.indexOf("don't()", ignoreCase = true)
+    return input.substring(0..<windowEnd)
+}
+
+private fun otherWindows(input: String): List<Int> {
+    val input = StringBuilder(input) // Explicit shadowing.
+    val sumList = mutableListOf<Int>()
+
+    while (input.isNotEmpty()) {
+        var start = input.indexOf("do()")
+        if (start == -1) break // No starting marker found.
+
+        var end = input.safeIndexOf("don't()")
+        if (end < start) { // don't() before do()
+            input.replace(0, start, "")
+            start = 0
+            end = input.safeIndexOf("don't()")
+        }
+
+        sumList.add(findValidOccurrences(input.substring(start, end)))
+        input.replace(0, end, "")
+    }
+
+    return sumList
+}
+
+private fun java.lang.StringBuilder.safeIndexOf(s: String): Int {
+    val potentialMatch = this.indexOf(s)
+    return if (potentialMatch == -1) {
+        this.lastIndex
+    } else {
+        potentialMatch
+    }
 }
 
 private fun findValidOccurrences(input: String): Int {
