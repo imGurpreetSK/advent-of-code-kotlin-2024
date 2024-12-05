2b0a2052af1c12ef157da90b4103a264ad0693a4
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-12-04 21:14:43 -0800

    [Day04] Cry through second part too.

--- src/Day04.kt
+++ src/Day04.kt
@@ -7,7 +7,7 @@ fun main() {
 
         for (y in 0..maxY) {
             for (x in 0..maxX) {
-                if (input[x][y] != 'X') continue
+                if (input[x][y] != 'A') continue
                 count += findOccurrencesCountAround(input, x to y, 0 to maxX, 0 to maxY)
             }
         }
@@ -16,11 +16,22 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0 // TODO(gs)
+        var count = 0
+        val maxX = input.firstOrNull()?.lastIndex ?: -1
+        val maxY = input.lastIndex
+
+        for (y in 0..maxY) {
+            for (x in 0..maxX) {
+                if (input[x][y] != 'A') continue
+                count += findOccurrencesCountAround(input, x to y, 0 to maxX, 0 to maxY)
+            }
+        }
+
+        return count
     }
 
-    val input = readInput("sample/Day04_part2")
-    part1(input).println()
+    val input = readInput("input/Day04")
+//    part1(input).println()
     part2(input).println()
 }
 
@@ -30,100 +41,48 @@ private fun findOccurrencesCountAround(
     xLimits: Pair<Int, Int>,
     yLimits: Pair<Int, Int>,
 ): Int {
-    val MAS = "MAS"
-    var count = 0
+    val map = HashMap<Move, Char>(4)
     Move.entries.forEach { move ->
         when (move) {
-            Move.UP -> {
-                if (coordinates.second - 3 >= yLimits.first) {
-                    var found = ""
-                    repeat(3) { index ->
-                        found += input[coordinates.first][coordinates.second + (move.yTravel * (index + 1))]
-                    }
-                    if (found == MAS) count++
-                }
-            }
-
-            Move.DOWN -> {
-                if (coordinates.second + 3 <= yLimits.second) {
-                    var found = ""
-                    repeat(3) { index ->
-                        found += input[coordinates.first][coordinates.second + (move.yTravel * (index + 1))]
-                    }
-                    if (found == MAS) count++
-                }
-            }
-
-            Move.LEFT -> {
-                if (coordinates.first - 3 >= xLimits.first) {
-                    var found = ""
-                    repeat(3) { index ->
-                        found += input[coordinates.first + (move.xTravel * (index + 1))][coordinates.second]
-                    }
-                    if (found == MAS) count++
-                }
-            }
-
-            Move.RIGHT -> {
-                if (coordinates.first + 3 <= xLimits.second) {
-                    var found = ""
-                    repeat(3) { index ->
-                        found += input[coordinates.first + (move.xTravel * (index + 1))][coordinates.second]
-                    }
-                    if (found == MAS) count++
-                }
-            }
-
             Move.UP_LEFT -> {
-                if (coordinates.second - 3 >= yLimits.first && coordinates.first - 3 >= xLimits.first) {
-                    var found = ""
-                    repeat(3) { iteration ->
-                        found += input[coordinates.first + (move.xTravel * (iteration + 1))][coordinates.second + (move.yTravel * (iteration + 1))]
-                    }
-                    if (found == MAS) count++
+                if (coordinates.second - 1 >= yLimits.first && coordinates.first - 1 >= xLimits.first) {
+                    map[move] = input[coordinates.first + move.xTravel][coordinates.second + move.yTravel]
                 }
             }
 
             Move.UP_RIGHT -> {
-                if (coordinates.second - 3 >= yLimits.first && coordinates.first + 3 <= xLimits.second) {
-                    var found = ""
-                    repeat(3) { iteration ->
-                        found += input[coordinates.first + (move.xTravel * (iteration + 1))][coordinates.second + (move.yTravel * (iteration + 1))]
-                    }
-                    if (found == MAS) count++
+                if (coordinates.second - 1 >= yLimits.first && coordinates.first + 1 <= xLimits.second) {
+                    map[move] = input[coordinates.first + move.xTravel][coordinates.second + move.yTravel]
                 }
             }
 
             Move.DOWN_LEFT -> {
-                if (coordinates.second + 3 <= yLimits.second && coordinates.first - 3 >= xLimits.first) {
-                    var found = ""
-                    repeat(3) { iteration ->
-                        found += input[coordinates.first + (move.xTravel * (iteration + 1))][coordinates.second + (move.yTravel * (iteration + 1))]
-                    }
-                    if (found == MAS) count++
+                if (coordinates.second + 1 <= yLimits.second && coordinates.first - 1 >= xLimits.first) {
+                    map[move] = input[coordinates.first + move.xTravel][coordinates.second + move.yTravel]
                 }
             }
 
             Move.DOWN_RIGHT -> {
-                if (coordinates.second + 3 <= yLimits.second && coordinates.first + 3 <= xLimits.second) {
-                    var found = ""
-                    repeat(3) { iteration ->
-                        found += input[coordinates.first + (move.xTravel * (iteration + 1))][coordinates.second + (move.yTravel * (iteration + 1))]
-                    }
-                    if (found == MAS) count++
+                if (coordinates.second + 1 <= yLimits.second && coordinates.first + 1 <= xLimits.second) {
+                    map[move] = input[coordinates.first + move.xTravel][coordinates.second + move.yTravel]
                 }
             }
         }
     }
 
-    return count
+    return if (map.values.count() == 4 &&
+        map.values.all { it == 'M' || it == 'S' } &&
+        !((map[Move.UP_LEFT] == 'M' && map[Move.DOWN_RIGHT] == 'M') ||
+                (map[Move.UP_RIGHT] == 'M' && map[Move.DOWN_LEFT] == 'M') ||
+                (map[Move.UP_LEFT] == 'S' && map[Move.DOWN_RIGHT] == 'S') ||
+                (map[Move.UP_RIGHT] == 'S' && map[Move.DOWN_LEFT] == 'S'))) {
+        1
+    } else {
+        0
+    }
 }
 
 private enum class Move(val xTravel: Int, val yTravel: Int) {
-    UP(0, -1),
-    DOWN(0, 1),
-    LEFT(-1, 0),
-    RIGHT(1, 0),
     UP_LEFT(-1, -1),
     UP_RIGHT(1, -1),
     DOWN_LEFT(-1, 1),
